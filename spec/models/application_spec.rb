110de23ebda39ec6b2e3afa528b0e4324d16fd95
Sergio Azcona <sergioazcona@Sergios-MBP.home>
2022-12-10 21:07:57 -0500

    test: default value test written

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -14,4 +14,14 @@ RSpec.describe Application, type: :model do
     it { should validate_numericality_of(:zip_code) }
     it { should validate_presence_of(:status) }
   end
+  
+  it 'has a default status value' do
+    @new_app = Application.new
+
+    expect(@new_app.status).to_not eq("Approved")
+    expect(@new_app.status).to_not eq("Rejected")
+    expect(@new_app.status).to_not eq("Pending")
+
+    expect(@new_app.status).to eq("In Progress")
+  end
 end
